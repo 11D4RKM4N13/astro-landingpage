bb971f2d2a84acf2bb64025adf0e0d37cc054478
Edwin Suarez <edwinsuarezb9@gmail.com>
2026-05-08 13:50:02 -0500

    [FEAT] Agrega https

--- src/config/site.mjs
+++ src/config/site.mjs
@@ -3,7 +3,7 @@ export const SITE = {
 	description:
 		"Fabricamos lanyards y cintas personalizadas para eventos, empresas y promociones en Colombia. Calidad garantizada y envío nacional.",
 	defaultLanguage: "es",
-	url: "www.cintasylanyards.com",
+	url: "https://cintasylanyards.com/",
 	author: "Edwin Suarez",
 	ogImage: "/og-image.png",
 	web3formsAccessKey: "c4d0565a-2556-48ce-8ee5-9290a351af95",
